e003d06fcac734e015a64fb085893a595f0c4220
JahfarEv <evjahfarali@gmail.com>
2023-12-06 07:36:00 +0530

    update

--- src/App.js
+++ src/App.js
@@ -8,23 +8,35 @@ function App() {
     main:{},
     weather:[{}]
   });
- useEffect(()=>{
-  async function getData(){
-    const Api = "https://api.openweathermap.org/data/2.5/weather?&appid=de7e9e5cabf1db28201cac646548c99e&q=Malappuram,India&units=metric"
-    try{
-      let response = await axios.get(Api)
-      setWeatherdata(response.data)
-      console.log(response);
-    }
-    catch(err){
-      console.log(err);
-    }
-  }
-  getData();
+  const [location,setLocation] = useState('')
+//  useEffect(()=>{
+//   async function getData(){
+    const Api = `https://api.openweathermap.org/data/2.5/weather?&appid=de7e9e5cabf1db28201cac646548c99e&q=${location},India&units=metric`
+//     try{
+//       let response = await axios.get(Api)
+//       setWeatherdata(response.data)
+//       console.log(response);
+//     }
+//     catch(err){
+//       console.log(err);
+//     }
+//   }
+//   getData();
 
- },[])
+//  },[])
+ const searchLocation = (event) =>{
+  if(event.key === 'Enter'){
+    axios.get(Api).then((response)=>{
+      setWeatherdata(response.data)
+      console.log(response.data);
+    })
+  }
+ }
   return (
     <div className="home-container">
+    <div className="search">
+      <input value={location} onChange={event => setLocation(event.target.value) } onKeyPress={searchLocation} placeholder="Enter Location" type="text" />
+    </div>
     <div className="main-container">
     
    <div className="card">
